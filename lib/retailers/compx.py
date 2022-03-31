103967e8bb5610d076e5970de2e292483e8490ba
Oleksandr Dunayevskyy <oleksandr.dunayevskyy@gmail.com>
2022-03-31 15:29:10 +0300

    clean up

--- lib/retailers/compx.py
+++ lib/retailers/compx.py
@@ -1,4 +1,3 @@
-from numpy import mat
 from lib.retailers.interface import Retailer
 from lib.common.fuzzy_matcher import matches_search_keyword, get_query_string_for_search_keyword
 import requests
@@ -14,14 +13,14 @@ class Compx(Retailer):
 
 
     def get_prices(self, search_keyword: str) -> list[tuple[str,float]]:
-        request = requests.Request('GET','https://compx.com.ua/katalog/search/filter/page=all;sort_price=ASC/', params={'q':get_query_string_for_search_keyword(search_keyword)}, )
+        request = requests.Request('GET','https://compx.com.ua/katalog/search/filter/page=all/', params={'q':get_query_string_for_search_keyword(search_keyword)}, )
         response = self._session.send(request.prepare())
         response.raise_for_status()
         parser = BeautifulSoup(response.text, 'html.parser')
         r = []
         for item_main_div in parser.find_all(class_='catalogCard-main'):
             title = item_main_div.find(class_='catalogCard-title').text.strip()
-            price = item_main_div.find(class_='catalogCard-price').text.strip().replace(' ','').replace('грн','')
+            price = item_main_div.find(class_='catalogCard-price').text.replace(' ','').replace('грн','').strip()
             if matches_search_keyword(title, search_keyword):
                 r.append((title,price))
         return r
