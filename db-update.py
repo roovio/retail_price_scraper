f30bd582f0ebad24f9aa222a7d42152e731b42e7
Oleksandr Dunayevskyy <oleksandr.dunayevskyy@gmail.com>
2022-04-20 13:33:54 +0300

    db-update: ensure snapsot is committed if an exception occurs during snapshot preparation

--- db-update.py
+++ db-update.py
@@ -7,19 +7,22 @@ from lib.common.db import Db
 def main():
     conf = yaml.safe_load(open('config/scraper.yml', 'r'))
 
-    retailer_factory =  RetailerFactory()
+    retailer_factory = RetailerFactory()
+
     db = Db()
     snapshot = db.create_snapshot_sink()
-    for query  in conf['search_keywords']:
-        print(query, end='',flush=True)
-        for name,retailer in retailer_factory.get_retailer_iterator() :
-            print('.', end='',flush=True)
-            scrape_result: list[ItemPosition] = retailer.get_prices(query)
-            for item_pos in scrape_result:
-                snapshot.add_data(retailer=name, query=query, title=item_pos.title, url=item_pos.url, price=item_pos.price )
-        print()
-    snapshot.commit()
+
+    try:
+        for query  in conf['search_keywords']:
+            print(query, end='',flush=True)
+            for name,retailer in retailer_factory.get_retailer_iterator() :
+                print('.', end='',flush=True)
+                scrape_result: list[ItemPosition] = retailer.get_prices(query)
+                for item_pos in scrape_result:
+                    snapshot.add_data(retailer=name, query=query, title=item_pos.title, url=item_pos.url, price=item_pos.price )
+            print()
+    finally:
+        snapshot.commit()
 
 if __name__ == '__main__':
     main()
-    
